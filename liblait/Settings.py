288198e0d768a6a406a5d3f1e25a416431647323
A.J. Venter <ajventer@gmail.com>
2017-11-30 18:41:10 +0200

    Started class for main menu environment

--- liblait/Settings.py
+++ liblait/Settings.py
@@ -3,26 +3,31 @@ import os
 import yaml
 
 class SETTINGS(object):
-	datadir = ''
-	gamedir = ''
-	settingsdict = {}
-	settingsfile = ''
-	fullscreen = False
-	res_x = 0
-	res_y = 0
-	def __init__(self, gamedir):
-		self.gamedir = gamedir
-		self.datadir = os.path.join(self.gamedir,data)
-		self.voicedir = os.path.join(os.path.join(self.datadir,'Sound'),'Voice')
-		settingsfile = os.path.join(self.gamedir,'settings.yml')
-		self.reload_settings()
-		self.res_x, self.res_y = self.settingsdict['Resolution'].split('x')
-		self.fullscreen = self.settingsdict['Fullscreen']
+    datadir = ''
+    gamedir = ''
+    settingsdict = {}
+    settingsfile = ''
+    fullscreen = False
+    borderless = False
+    res_x = 0
+    res_y = 0
+    def __init__(self, gamedir):
+        self.gamedir = gamedir
+        self.datadir = os.path.join(self.gamedir,'data')
+        self.voicedir = os.path.join(os.path.join(self.datadir,'Sound'),'Voice')
+        self.settingsfile = os.path.join(self.gamedir,'settings.yml')
+        print self.settingsfile
+        self.reload_settings()
 
-	def reload_settings(self):
-		self.settingsdict = yaml.safe_load(self.settingsfile)
+    def reload_settings(self):
+        self.settingsdict = yaml.safe_load(open(self.settingsfile))
+        self.res_x = self.settingsdict['Resolution']['w']
+        self.res_y = self.settingsdict['Resolution']['h']
+        self.fullscreen = self.settingsdict['Fullscreen']
+        self.borderless = self.settingsdict['Borderless']
 
-	def save_settings(self):
-		open(self.settingsfile,'w').write(yaml.dump(self.settingsdict,os.path.join(self.gamedir,'settings.yml')))
+    def save_settings(self):
+        open(self.settingsfile,'w').write(yaml.dump(self.settingsdict, default_flow_style=False))
+        self.reload_settings()
 
 
---
+++ liblait/mainmenu.py
@@ -0,0 +1,9 @@
+import pygame
+from pygame.locals import *
+
+class mainMenu(object):
+	def __init__(screen, settings):
+		self.screen = screen
+
+	def run():
+		return 'quit'
