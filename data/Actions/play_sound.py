014a407d9bbd6c83ab059cb91a7fa06f050aab8c
A.J. Venter <ajventer@gmail.com>
2017-12-19 13:35:13 +0200

    Added sound effect to level 1 spellbook

--- data/Actions/play_sound.py
+++ data/Actions/play_sound.py
@@ -3,6 +3,6 @@ from pygame.locals import *
 import os
 
 def collision(me, collider, settings, sndtype, sndfile):
-        sound = pygame.mixer.Sound(os.path.join({'voice':settings.voicedir, 'fx': settings.fxdir, 'guifx': settings.guifxdir}[sndtype],sndfile))
-        sound.set_volume(settings.voicevol)
-        sound.play()
+    sound = pygame.mixer.Sound(os.path.join({'voice':settings.voicedir, 'fx': settings.fxdir, 'guifx': settings.guifxdir}[sndtype],sndfile))
+    sound.set_volume(settings.voicevol)
+    sound.play()
