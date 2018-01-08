90be57ad1ceeeb984ff565c29a4a0a95a7c32e78
A.J. Venter <ajventer@gmail.com>
2018-01-08 18:01:45 +0200

    Add thorpy data into build because pyinstaller doesn't

--- winbuild.bat
+++ winbuild.bat
@@ -1,4 +1,8 @@
 
-pyinstaller --workpath ../laitbuild/build --distpath ../laitbuild -y --clean --onedir --add-data Config;Config  --add-data data;data -i data/winicon/lait.ico --noconsole --noupx lait lait_editor
+pyinstaller --workpath ../laitbuild/build --distpath ../laitbuild -y --clean --onedir --add-data Config;Config  --add-data data;data -i data/winicon/lait.ico --noupx --noconsole lait lait_editor
 
 copy settings.yml ..\laitbuild\lait
+mkdir ..\laitbuild\lait\thorpy
+mkdir ..\laitbuild\lait\thorpy\data
+
+copy C:\Python36\Lib\site-packages\thorpy\data  ..\laitbuild\lait\thorpy\data\
